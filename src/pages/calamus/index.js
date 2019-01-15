8c0baaf67ca4c38c0a74c5d020d994f5c6e1c639
calamus0427 <1211994507@qq.com>
2019-01-15 13:56:45 +0800

    taro-demo

---
+++ src/pages/calamus/index.js
@@ -0,0 +1,50 @@
+import Taro, { Component } from '@tarojs/taro'
+import { View, Button, Text } from '@tarojs/components'
+import { connect } from '@tarojs/redux'
+
+import { add, minus, asyncAdd } from '../../actions/counter'
+
+import './index.scss'
+
+
+@connect(({ counter }) => ({
+  counter
+}), (dispatch) => ({
+  add () {
+    dispatch(add())
+  },
+  dec () {
+    dispatch(minus())
+  },
+  asyncAdd () {
+    dispatch(asyncAdd())
+  }
+}))
+class Calamus extends Component {
+
+    config = {
+    navigationBarTitleText: 'Calamus'
+  }
+
+  componentWillReceiveProps (nextProps) {
+    console.log(this.props, nextProps)
+  }
+
+  componentWillUnmount () { }
+
+  componentDidShow () { }
+
+  componentDidHide () { }
+
+  render () {
+    return (
+      <View className='calamus'>
+        <View><Text>{this.props.counter.num}</Text></View>
+        <View><Text>Hello, page</Text></View>
+      </View>
+    )
+  }
+}
+
+export default Index
+index.scss
